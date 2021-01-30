87da64307e737b3861fedf9fa5f1fde9bb4dd319
yusuf balci <yusufbasribalci@gmail.com>
2021-01-30 11:22:02 +0300

    Query

---
+++ SQLQuery3.sql
@@ -0,0 +1,54 @@
+﻿select ProductID ,ProductName from Products
+
+-- Orderıd toplam tutar
+select OrderID ,SUM( UnitPrice*(1.0- Discount)*Quantity) as "Kazanılan Toplam Miktar"  from [Order Details] GROUP BY OrderID
+
+
+-- ProductID tutar
+select ProductID ,
+SUM(UnitPrice*(1.0- Discount)*Quantity) as "Kazanılan Toplam Miktar" 
+from [Order Details] 
+GROUP BY ProductID 
+
+
+
+select * from [Order Details]
+select * from Orders
+
+
+select Products.ProductName 'Ürün Adı', 
+sum([Order Details].Quantity * [Order Details].UnitPrice) 'Kazanılan Toplam Miktar' 
+from Products inner join [Order Details]
+on Products.ProductID=[Order Details].ProductID inner join ORDERS 
+on Orders.OrderID=[Order Details].OrderID
+group by Products.ProductName order by  ProductName
+
+,
+
+-- Order toplam tutar
+select OrderID ,SUM( UnitPrice*(1.0- Discount)*Quantity) as "Kazanılan Toplam Miktar"  from [Order Details] GROUP BY OrderID
+
+
+-- ProductID tutar
+select ProductID ,
+SUM(UnitPrice*(1.0- Discount)*Quantity) as "Kazanılan Toplam Miktar" 
+from [Order Details] 
+GROUP BY ProductID 
+
+-- ProductName tutar
+select Products.ProductName ,
+SUM([Order Details].UnitPrice*(1.0- [Order Details].Discount)*[Order Details].Quantity) as "Kazanılan Toplam Miktar" 
+from Products inner join [Order Details]
+on Products.ProductID=[Order Details].ProductID inner join ORDERS 
+on Orders.OrderID=[Order Details].OrderID
+GROUP BY Products.ProductName order by  ProductName
+
+-- Order toplam tutar
+select OrderID ,SUM( UnitPrice*(1.0- Discount)*Quantity) as "Kazanılan Toplam Miktar"  from [Order Details] GROUP BY OrderID
+
+--Yükleme Yeri bazında toplam tutar
+select ShipName,
+SUM([Order Details].UnitPrice*(1.0- [Order Details].Discount)*[Order Details].Quantity) as "Kazanılan Toplam Miktar" 
+from [Order Details] inner join orders
+on orders.OrderID=[Order Details].OrderID 
+GROUP BY ShipName
